3a027e0ef6e80800696a59c9c4f93d932ca4b8b6
tobideanna <deannapradatobias@gmail.com>
2024-09-17 12:37:40 -0400

    added buttons to the front end, and added functionality to play the game with the buttons instead of the console. removed console game functionality.

--- rps.js
+++ rps.js
@@ -2,64 +2,46 @@ console.log("JavaScript is linked!");
 
 let humanScore = 0;
 let computerScore = 0;
+const buttons = document.getElementById("buttons");
 
 function getComputerChoice (){
     let computerChoice = Math.floor(Math.random () * 3);
     if (computerChoice === 0){
-        computerChoice = "Rock";
+        computerChoice = "rock";
         } else if (computerChoice === 1){
-            computerChoice = "Paper";
+            computerChoice = "paper";
         } else {
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
         }
         
     console.log(computerChoice);
     return computerChoice;
 }
+buttons.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    playRound(e.target.id);
+});
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors? R, P or S");
-    let humanUpper = humanChoice.toUpperCase();
-    if (humanUpper === "R") {
-        humanUpper = "Rock";
-    } else if (humanUpper === "P") {
-        humanUpper = "Paper";
-    } else if (humanUpper === "S") {
-        humanUpper = "Scissors";
-    }
-    console.log(humanUpper);
-    return humanUpper;
-}
-
-function playRound(){
-    let computerScore = 0;
-    let humanScore = 0;
-    while (humanScore < 3 && computerScore < 3){
-        let humanUpper = getHumanChoice();
+function playRound(humanChoice){
         let computerChoice = getComputerChoice();
-        if (humanUpper === computerChoice){
+        if (humanChoice === computerChoice){
             console.log("It's a tie!");
-        } else if ((humanUpper === "Rock" && computerChoice === "Paper") ||
-                   (humanUpper === "Paper" && computerChoice === "Scissors") ||
-                   (humanUpper === "Scissors" && computerChoice === "Rock")){
-            console.log(`You lose! ${computerChoice} beats ${humanUpper}`);
+        } else if ((humanChoice === "rock" && computerChoice === "paper") ||
+                   (humanChoice === "paper" && computerChoice === "scissors") ||
+                   (humanChoice === "scissors" && computerChoice === "rock")){
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
             computerScore += 1;
-        } else if ((humanUpper === "Rock" && computerChoice === "Scissors") ||
-                   (humanUpper === "Paper" && computerChoice === "Rock") ||
-                   (humanUpper === "Scissors" && computerChoice === "Paper")){
-            console.log(`You win! ${humanUpper} beats ${computerChoice}`);
+        } else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+                   (humanChoice === "paper" && computerChoice === "rock") ||
+                   (humanChoice === "scissors" && computerChoice === "paper")){
+            console.log(`You win! ${humanChoice} beats ${computerChoice}`);
             humanScore += 1;
-        } else {
-            console.log("Invalid input. Please enter R, P, or S.");
         } 
-        
         console.log(`Human score: ${humanScore}\nComputer score: ${computerScore}`);
-    }
+
     if (humanScore > computerScore){
         console.log("You win the game!");
     } else {
         console.log("You lose the game!");
     }
 }
-
-playRound();
